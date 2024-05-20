9167735c90fbfd7f8aa560a6811b6745117a1193
Trim21 <trim21.me@gmail.com>
2024-05-21 05:58:13 +0800

    fix source build

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from glob import glob
 
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 
 module = Extension(
     "bencode_c._bencode",
-    sources=glob("./src/bencode_c/*.[ch]"),
+    sources=glob("./src/bencode_c/*.c"),
     include_dirs=["./src/bencode_c"],
 )
 
 setup(
     ext_modules=[module],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
 )
