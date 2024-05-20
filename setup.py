71ef2db46f7b891f4457efa1e39ecaa65256d27a
Trim21 <trim21.me@gmail.com>
2024-05-21 07:52:47 +0800

    build: abi3 for py39

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import sys
 from glob import glob
 
 from setuptools import setup, Extension, find_packages
@@ -6,6 +7,7 @@ module = Extension(
     "bencode_c._bencode",
     sources=glob("./src/bencode_c/*.c"),
     include_dirs=["./src/bencode_c"],
+    py_limited_api=True,
 )
 
 setup(
@@ -13,5 +15,5 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     package_data={"": ["*.h", '*.c', '*.pyi', 'py.typed']},  # <- This line
-    options={'bdist_wheel': {'py_limited_api': 'cp38'}},
+    options={'bdist_wheel': {'py_limited_api': f'cp3{sys.version_info[1]}'}},
 )
