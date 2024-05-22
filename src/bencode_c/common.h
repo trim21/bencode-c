a93085008b16af37f506cc8a79b275660230ad1d
Trim21 <trim21.me@gmail.com>
2024-05-23 04:54:44 +0800

    ci: add format check

--- src/bencode_c/common.h
+++ src/bencode_c/common.h
@@ -5,9 +5,10 @@
 #define HPy_ssize_t Py_ssize_t
 #define HPy PyObject *
 
-#define NON_SUPPORTED_TYPE_MESSAGE                                                                                     \
-  "invalid type '%s', "                                                                                                \
-  "bencode only support bytes, str, int, list, tuple, dict and bool(encoded as 0/1, decoded as int)"
+#define NON_SUPPORTED_TYPE_MESSAGE                                                                 \
+  "invalid type '%s', "                                                                            \
+  "bencode only support bytes, str, "                                                              \
+  "int, list, tuple, dict and bool(encoded as 0/1, decoded as int)"
 
 #define MY_COMMON_H
 #endif
